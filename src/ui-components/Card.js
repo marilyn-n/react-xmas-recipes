733de53774a99f15dff5a4ddf7b60e211eccf8e0
marilyn-n <negrete.aguilar.m@gmail.com>
2021-01-13 21:28:20 -0500

    add minor changes to card component

--- src/ui-components/Card.js
+++ src/ui-components/Card.js
@@ -1,16 +1,20 @@
 
 function Card() {
   return (
-    <div className="recipes">
-        <div className="card recipe">
-            <i class="far fa-heart recipe--like"></i>
-            <img src="https://slcwesthsmtb.com/wp-content/uploads/2019/09/placeholder.png" className="card-img-top" alt="..."/>
-            <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-        </div>
-    </div>
+      <div className="card recipe">
+          <i class="far fa-heart recipe--like"></i>
+          <img src="https://slcwesthsmtb.com/wp-content/uploads/2019/09/placeholder.png" className="card-img-top" alt="..."/>
+          <div className="card-body recipe__body">
+              <h5 className="recipe__title">Card title</h5>
+              <div className="recipe__rating">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+              </div>
+              <p className="recipe--text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+      </div>
   );
 }
 
